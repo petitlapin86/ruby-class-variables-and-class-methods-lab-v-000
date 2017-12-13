9bce3ece6b647939420d91397cf2daadc1b4574d
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-13 14:07:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,6 +17,22 @@ def self.count
 @@count #keeping track of itself as a class
 end
 
+def self.artists #method returns an array of ONLY UNIQUE artists, no duplicates.
+    @@artists.uniq
+  end
+
+  def self.artist_count
+    artist_count = {} #creates a hash
+    @@artists.each do |artist| #itterate over the array
+          if artist_count[artist] #check to see if the hash already contains a key of a particular genre.
+            artist_count[artist] += 1 #If so, increment the value of that key by one
+          else
+            artist_count[artist] = 1 #otherwise, create a new key/value pair
+          end
+        end
+        artist_count
+  end
+
 def self.genres #method returns an array of ONLY UNIQUE genres, no duplicates.
     @@genres.uniq
   end
@@ -33,21 +49,9 @@ def self.genre_count
        genre_count
      end
 
-def self.artists #method returns an array of ONLY UNIQUE artists, no duplicates.
-    @@artists.uniq
-  end
 
-def self.artist_count
-  artist_count = {} #creates a hash
-  @@artists.each do |artist| #itterate over the array
-        if artist_count[artist] #check to see if the hash already contains a key of a particular genre.
-          artist_count[artist] += 1 #If so, increment the value of that key by one
-        else
-          artist_count[artist] = 1 #otherwise, create a new key/value pair
-        end
-      end
-      artist_count
-end
+
+
 
 
 end
