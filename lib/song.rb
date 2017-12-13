a521879715949a75f660cb47c10af64dc6e237eb
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-13 14:11:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
-        artist_count[artist] += 1 
+        artist_count[artist] += 1
       else
         artist_count[artist] = 1
       end
@@ -42,7 +42,7 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count[genre] += 1 
+        genre_count[genre] += 1
       else
         genre_count[genre] = 1
       end
